a884b3243ac29a03d8d4dbc3d09d50a5ef339585
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-12-03 16:59:50 -0300

    Create endpoints for server

---
+++ server.js
@@ -0,0 +1,36 @@
+const PORT = 3000;
+const HTTP = require('http');
+
+function handler(req, res) {
+    // release for cors
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // header to send response
+    const send = (payload = {}, statusCode = 200) => {
+        res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify(payload))
+    };
+
+    // router
+    switch(req.url) {
+        case '/':
+            send({ message: `You are on /`});
+        break;
+
+        case '/status':
+            send({ message: `The server is running`, uptime: process.uptime() });
+        break;
+
+        default:
+            send({ message: 'Resource not found' }, 404);
+        break;
+    }
+
+    res.end();
+}
+
+HTTP
+    .createServer(handler)
+    .listen(PORT, () => {
+        console.log(`The API is running on port: ${PORT}.`)
+    });
