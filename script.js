73e98d27224e8472fcee5fa6aa025f4ce80b9a1f
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2022-12-03 17:00:49 -0300

    Creat script for request

---
+++ script.js
@@ -0,0 +1,21 @@
+const API_ADDRESS = 'http://localhost:3000';
+    
+async function get(endpoint) {
+  return fetch(API_ADDRESS+endpoint).then(resp => resp.json());
+}
+
+function yell(endpoint) {
+  loading(true);
+
+  get(endpoint)
+    .then(data => {
+      alert(`Message: ${data?.message}.\n\nCheck the console for data`);
+      console.log(data);
+    })
+    .catch(console.error)
+    .finally(() => loading(false));
+}
+
+function loading(is = true) {
+  document.body.classList[is ? 'add' : 'remove']('loading');
+} 
